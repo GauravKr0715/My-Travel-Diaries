e1eba7d4d27cc022ab42c520c314e95dadbc8551
Gaurav_kR <laxaman.pal@gmail.com>
2020-10-31 22:57:35 +0530

    change for mapbox API

--- client/src/components/MapBoxComponent.js
+++ client/src/components/MapBoxComponent.js
@@ -58,7 +58,7 @@ const MapBoxComponent = ({ token }) => {
             <ReactMapGL
             {...viewport}
             mapStyle="mapbox://styles/gauravkr-0715/ckdd36foi1klc1inz3eze4t62"
-            mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
+            mapboxApiAccessToken="pk.eyJ1IjoiZ2F1cmF2a3ItMDcxNSIsImEiOiJja2RkMXZ0aWYxcDU3MnJrNnQ5cTFrMmhjIn0.oQrDvDe-reuozctDgTviAw"
             onViewportChange={setViewport}
             onDblClick={showAddMarkerPopup}
             >
